4a636daa64315d9fd165ba75e2089a7a61e2e0a1
Trent Stromkins <trent@smallrobot.org>
2018-08-29 17:05:41 -0700

    head data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,7 @@
+import { setProperties } from '@ember/object';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  headData: service(),
 });
